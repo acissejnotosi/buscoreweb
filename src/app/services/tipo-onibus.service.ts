34c07399c195fa33080bd8d739007d3b75b41e3c
Cesare Cibien <cesa.cibien@gmail.com>
2018-12-12 23:10:45 -0200

    Formulario nao funcional da tela de Linhas

--- src/app/services/tipo-onibus.service.ts
+++ src/app/services/tipo-onibus.service.ts
@@ -1,23 +1,21 @@
 import { Injectable } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs'
-import { ILinha } from '../models/linha.model'
 import { environment } from '../../environments/environment'
+import { ITipoOnibus } from '../models/tipo-onibus.model'
 
 @Injectable({
 	providedIn: 'root'
 })
-export class LinhaService {
+export class TipoOnibusService {
 
-	private url: string = "api/linha"
+	private url: string = "api/tipoOnibus"
 	private api_url: string = environment.api_url
 	private final_url: string = this.api_url.concat(this.url)
 
 	constructor(private _http: HttpClient) { }
 
-	getLinhas(): Observable<ILinha[]> {
-		return this._http.get<ILinha[]>(this.final_url)
+	getTipoOnibus(): Observable<ITipoOnibus[]> {
+		return this._http.get<ITipoOnibus[]>(this.final_url)
 	}
-
-
 }
