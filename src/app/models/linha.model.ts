5d09358fe7a987f3007f0028f85cefef91a79a32
Cesare Cibien <cesarecibien@Cesares-MacBook-Pro.local>
2018-12-15 22:41:36 -0200

    Adicionando tela de edicao visualizacao da linha

--- src/app/models/linha.model.ts
+++ src/app/models/linha.model.ts
@@ -1,17 +1,19 @@
 export interface ILinha {
-	linhaId: number
-	dataCadastro: Date
-	numeroLinha: number
-	nomeLinha: string
-	numParadas: number
-	numBuracos: number
-	numLombadas: number
-	numSemaforo: number
-	totalRPNFreiosFabrica: number
-	totalRPNEmbreagemFabrica: number
-	totalRPNSuspensaoFabrica: number
-	totalKmFreiosFabrica: number
-	totalKmEmbreagemFabrica: number
-	totalKmSuspensaoFabrica: number
-	tipoOnibusId: number
+	linhaId: number;
+	dataCadastro: Date;
+	numeroLinha: number;
+	nomeLinha: string;
+	numParadas: number;
+	numBuracos: number;
+	numLombadas: number;
+	numSemaforo: number;
+	totalRPNFreiosFabrica: number;
+	totalRPNEmbreagemFabrica: number;
+	totalRPNSuspensaoFabrica: number;
+	totalKmFreiosFabrica: number;
+	totalKmEmbreagemFabrica: number;
+	totalKmSuspensaoFabrica: number;
+	tipoOnibusId: number;
+	tipoOnibusNome: string;
+	pesoOnibus: number;
 }
--- src/app/view-line/view-line.component.ts
+++ src/app/view-line/view-line.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { LinhaService } from '../services/linha.service'
+import { ILinha } from '../models/linha.model'
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-view-line',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewLineComponent implements OnInit {
 
-  constructor() { }
+  linhas: ILinha[]
+  constructor(
+    private _linhaService: LinhaService,
+    private _tostrService: ToastrService) { }
 
   ngOnInit() {
+    this._linhaService.getLinhas().subscribe(
+      result => {
+        this.linhas = result
+      },
+      error => {
+        console.log(error)
+          this._tostrService.error(error.message, "Ocorreu um erro ao carregar as linhas")
+      }
+    )
   }
 
+  removerLinha(linha: ILinha) {
+    console.log(linha)
+  }
 }
