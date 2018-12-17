79dc04499cace6757980b71c9f8377b6021f1a6c
Jessica Isoton <Jessica Isoton@DESKTOP-4KP64Q3>
2018-12-16 23:30:25 -0200

    t

--- src/app/rpncalculator/rpncalculator.component.ts
+++ src/app/rpncalculator/rpncalculator.component.ts
@@ -10,11 +10,32 @@ import { ILinha } from '../models/linha.model';
 export class RPNCalculatorComponent implements OnInit {
 
 	linhas: ILinha[]
+	ocor1: number
+	seve1: number
+	dete1: number
+	ocor2: number
+	seve2: number
+	dete2: number
+	ocor3: number
+	seve3: number
+	dete3: number
+	RPNT1: number
+	RPNT2: number
+	RPNT3: number
 
 	constructor(private _linhaService: LinhaService) { }
 
 	ngOnInit() {
 		this.getLinhas()
+		this.ocor1 = 0
+		this.seve1 = 0
+		this.dete1 = 0
+		this.ocor2 = 0
+		this.seve2 = 0
+		this.dete2 = 0
+		this.ocor3 = 0
+		this.seve3 = 0
+		this.dete3 = 0
 	}
 
 
@@ -28,6 +49,20 @@ export class RPNCalculatorComponent implements OnInit {
 				console.log(error)
 			}
 		)
+	}
 
+	setRPN1(){
+		this.RPNT1 =
+			(this.ocor1 * this.seve1 * this.dete1) + 
+			(this.ocor2 * this.seve2 * this.dete2) +
+			(this.ocor3 * this.seve3 * this.dete3)
+	}
+
+	setRPN2(){
+		this.RPNT2 = 0
+	}
+
+	setRPN(){
+		this.RPNT3 = 0
 	}
 }
